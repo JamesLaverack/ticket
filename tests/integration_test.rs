3a7ae12892b4b464984b724d6e722bc998480f6a
James Laverack <james@jameslaverack.com>
2018-05-15 00:40:02 +0100

    Add test for init success

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -5,6 +5,8 @@ use tempfile::tempdir;
 use std::env;
 use std::path::PathBuf;
 use std::io::Result;
+use std::fs::File;
+use std::io::prelude::*;
 
 /// We need the absolute path to the ticket binary under test. We can't use
 /// Assert::main_binary or similar because we need to manipulate the current
@@ -28,4 +30,27 @@ fn init_fails_outside_of_git() {
         .and()
         .stderr().is("Failed to install ticket: Can't find a git repository from the current directory.")
         .unwrap();
+}
+
+#[test]
+fn init_passes_in_git() {
+    let dir = tempdir().unwrap();
+
+    // Init a git repo. This means this machine needs a `git` binary on the PATH
+    assert_cli::Assert::command(&["git"])
+        .with_args(&["init"])
+        .current_dir(dir.path())
+        .unwrap();
+
+    assert_cli::Assert::command(&[ticket_binary().unwrap()])
+        .with_args(&["init"])
+        .current_dir(dir.path())
+        .stdout().contains("Ticket git hook installed, happy hacking!")
+        .unwrap();
+
+    let mut file = File::open(dir.path().join(".git").join("hooks").join("prepare-commit-msg")).expect("Unable to open the file");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).expect("Unable to read the file");
+
+    assert_eq!("#!/bin/sh\nticket insert-ticket-reference $1\n", contents);
 }
