7dff9ee498be476f45211bad31a2f1ce8b8ab91f
James Laverack <james@jameslaverack.com>
2018-05-15 00:18:14 +0100

    Pull ticket binary path into function

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -2,18 +2,26 @@ extern crate tempfile;
 extern crate assert_cli;
 
 use tempfile::tempdir;
+use std::env;
+use std::path::PathBuf;
+use std::io::Result;
+
+/// We need the absolute path to the ticket binary under test. We can't use
+/// Assert::main_binary or similar because we need to manipulate the current
+/// directory that we execute in.const ticket_binary: 
+fn ticket_binary() -> Result<PathBuf> {
+    Ok(env::current_dir()?
+        .join("target")
+        .join("debug")
+        .join("ticket"))
+}
 
 #[test]
 fn init_fails_outside_of_git() {
     // There should be no git repo in this tempdir, unless someone has done `git init` in their `/tmp`
     let dir = tempdir().unwrap();
-    
-    // We need the absolute path to the ticket binary under test. We can't use
-    // Assert::main_binary or similar because we need to manipulate the current
-    // directory that we execute in.
-    let ticket_binary = std::env::current_dir().unwrap().join("target").join("debug").join("ticket");
-    
-    assert_cli::Assert::command(&[ticket_binary])
+
+    assert_cli::Assert::command(&[ticket_binary().unwrap()])
         .with_args(&["init"])
         .current_dir(dir.path())
         .fails()
